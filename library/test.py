d1b02578df26f60b1575509124772c89d7a30d96
Sandy Macdonald <sandyjmacdonald@gmail.com>
2019-02-12 10:52:22 +0000

    Improving test script

--- library/test.py
+++ library/test.py
@@ -20,19 +20,21 @@ colours = {"red": 1, "ir": 2, "green": 3}
 hr = HeartRate(max30105)
 
 try:
-    print("Temperature: {:.2f}".format(max30105.get_temperature()))
+    print("Temperature: {:.2f}C".format(max30105.get_temperature()))
     for c in colours:
         print("\nLighting {} LED".format(c.upper()))
         max30105.set_led_pulse_amplitude(colours[c], 12.5)
         time.sleep(2.5)
         print("Reading {} LED".format(c.upper()))
-        for i in range(20):
+        i = 0
+        while i < 10:
             samples = max30105.get_samples()
             if samples is not None:
                 ir = samples[colours[c] - 1] & 0xff
                 d = hr.low_pass_fir(ir)
                 print(d)
-                time.sleep(0.05)
+                time.sleep(0.1)
+                i += 1
         max30105.set_led_pulse_amplitude(colours[c], 0.0)
     print("\nTEST COMPLETE!!!")
 
